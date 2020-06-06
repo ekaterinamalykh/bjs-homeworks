b7cbd4227c19e0d5a1710214ceaa3779592ba05e
Ekaterina Malykh <minikat21@gmail.com>
2020-06-06 20:10:44 +0300

    first commit variables

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -7,21 +7,33 @@
 
 function averageMark(a, g, p) {
 	
-    // реализуйте здесь задание №1
-    
-	return // ваш ответ;
+	let algebra = a;
+	let geography = g;
+	let physics = p;
+	let average = (algebra + geography + physics) / 3;
+	
+	return average;
+	
 }
 
 function sayHello(userName) {
 	
-    // реализуйте здесь задание №2
-    
-	return // ваш ответ;
+	let myName = userName;
+	
+	return `Привет, мир! Меня зовут ${myName}`;
 }
 
 function calculateFormula() {
     
-	// реализуйте здесь задание №3
+	let x;
+	let y;
+	let z;
+
+	x = 2;
+	y = 22;
+	z = 0;
+
+	let result = x * y + 5 * z + x - 1;
     
-    return // ваш ответ;
+	return result;
 }
