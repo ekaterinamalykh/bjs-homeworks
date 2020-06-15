272140fa373c8b0ae421ad1403675bc7a138b0fd
Ekaterina Malykh <minikat21@gmail.com>
2020-06-15 20:42:36 +0300

    first commit wrappers-for-primitives

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,40 @@
+'use strict'
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+
+    if (isNaN(percent)) {
+        return `Параметр "процентная ставка" содержит неправильное значение ${percent}`;
+    } else if (isNaN(contribution)) {
+        return `Параметр "сумма первоначального взноса" содержит неправильное значение ${contribution}`;
+    } else if (isNaN(amount)) {
+        return `Параметр "сумма кредита" содержит неправильное значение ${amount}`;
+    } else if (isNaN(date)) {
+        return `Параметр "дата окончания кредита" содержит неправильное значение ${date}`;
+    } else {
+        
+    let loan = amount - contribution;
+    let now = new Date();
+    let persentageRate = percent / 100 / 12;
+    let totalMonths = Math.round((Date.parse(date) - Date.parse(now)) / 86400000 / 30);
+    let monthlyPayment = loan * (persentageRate + persentageRate / ((Math.pow(1 + persentageRate, totalMonths) - 1)));
+    let totalAmount = Math.floor(monthlyPayment * totalMonths * 100) / 100;
+
+    console.log(totalAmount);
+
+    return totalAmount;
+
+    }
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+
+    let greeting = `Привет, мир! Меня зовут ${name}`;
+
+    if (name === null) {
+        name = 'Аноним';
+    }
+    console.log(greeting);
+    return greeting;
+    
+    
 }
