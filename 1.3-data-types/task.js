927b5c8a0546f8ac9e50eb5015b6a23864a21576
Ekaterina Malykh <minikat21@gmail.com>
2020-06-19 18:22:11 +0300

    second commit wrappers-for-primitives

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -2,39 +2,38 @@
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
 
-    if (isNaN(percent)) {
-        return `Параметр "процентная ставка" содержит неправильное значение ${percent}`;
-    } else if (isNaN(contribution)) {
-        return `Параметр "сумма первоначального взноса" содержит неправильное значение ${contribution}`;
-    } else if (isNaN(amount)) {
-        return `Параметр "сумма кредита" содержит неправильное значение ${amount}`;
-    } else if (isNaN(date)) {
-        return `Параметр "дата окончания кредита" содержит неправильное значение ${date}`;
-    } else {
-        
-    let loan = amount - contribution;
-    let now = new Date();
-    let persentageRate = percent / 100 / 12;
-    let totalMonths = Math.round((Date.parse(date) - Date.parse(now)) / 86400000 / 30);
-    let monthlyPayment = loan * (persentageRate + persentageRate / ((Math.pow(1 + persentageRate, totalMonths) - 1)));
-    let totalAmount = Math.floor(monthlyPayment * totalMonths * 100) / 100;
+  if (isNaN(percent)) {
+    return `Параметр "процентная ставка" содержит неправильное значение ${percent}`;
+  }
 
-    console.log(totalAmount);
+  if (isNaN(contribution)) {
+    return `Параметр "сумма первоначального взноса" содержит неправильное значение ${contribution}`;
+  }
 
-    return totalAmount;
+  if (isNaN(amount)) {
+    return `Параметр "сумма кредита" содержит неправильное значение ${amount}`;
+  }
 
-    }
+  if (isNaN(date)) {
+    return `Параметр "дата окончания кредита" содержит неправильное значение ${date}`;
+  }
+
+  let loan = amount - contribution;
+  let now = new Date();
+  let persentageRate = percent / 100 / 12;
+  let totalMonths = Math.round((Date.parse(date) - Date.parse(now)) / 86400000 / 30);
+  let monthlyPayment = loan * (persentageRate + persentageRate / ((Math.pow(1 + persentageRate, totalMonths) - 1)));
+  let totalAmount = Math.floor(monthlyPayment * totalMonths * 100) / 100;
+  
+  console.log(totalAmount);
+
+  return totalAmount;
+
 }
+
 
 function getGreeting(name) {
 
-    let greeting = `Привет, мир! Меня зовут ${name}`;
+  return (`Привет, мир! Меня зовут ${name || "Аноним"}`);
 
-    if (name === null) {
-        name = 'Аноним';
-    }
-    console.log(greeting);
-    return greeting;
-    
-    
 }
