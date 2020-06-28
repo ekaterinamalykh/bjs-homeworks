6ecf80e5b3430ab522c496dd05432b95d9d893bb
Ekaterina Malykh <minikat21@gmail.com>
2020-06-28 18:20:10 +0300

    first commit object-prototype

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,33 @@
-//String.prototype.isPalindrome - для задачи №1
+"use strict";
 
-function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+// задача №1
+String.prototype.isPalindrome = function () {
+  
+  let str = this.toLowerCase().split('').reverse().join('');
+  return str.split(' ').join('') === this.toLowerCase().split(' ').join('') ? true : false;
 }
 
+
+// задача №2
+function getAverageMark(marks) {
+
+  let sumOfMarks = 0;
+  for (let i = 0; i < marks.length; i++) {
+    sumOfMarks += marks[i];
+  }
+
+  let average = sumOfMarks / marks.length;
+  let roundedAverage = Math.round(average);
+  return roundedAverage;
+}
+
+
+// задача №3
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+  
+  let now = new Date();
+  let bday = new Date(birthday);
+  let diff = Date.parse(now) - Date.parse(bday);
+  let age = diff / 31536000000;
+  return age >= 18 ? true : false;
 }
