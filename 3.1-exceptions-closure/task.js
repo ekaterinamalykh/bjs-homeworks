0aab023ed883fcc3d434ee04cfa37da822945ceb
ekaterinamalykh <minikat21@gmail.com>
2020-07-23 17:21:45 +0300

    second commit

--- 3.1-exceptions-closure/task.js
+++ 3.1-exceptions-closure/task.js
@@ -3,17 +3,16 @@
 //задача №1
 
 function parseCount(value) {
-  if (isNaN(Number.parseInt(value))) {
+  const parsed = Number.parseInt(value);
+  if (isNaN(parsed)) {
     throw new Error("Невалидное значение");
   }
-  const parsed = Number.parseInt(value);
   return parsed;
 }
 
 function validateCount(value) {
   try {
-    const parseResult = parseCount(value);
-    return parseResult;
+    return parseCount(value);
   } catch (e) {
     return Error("Невалидное значение");
   }
@@ -26,25 +25,23 @@ class Triangle {
     this.a = a;
     this.b = b;
     this.c = c;
-    if (this.a + this.b < this.c || this.a + this.c < this.b || this.b + this.c < this.a) {
+    if (a + b <= c || a + c <= b || b + c <= a) {
       throw new Error("Треугольник с такими сторонами не существует");
     }
   }
 
   getPerimeter() {
-    const perimeter = parseFloat(this.a + this.b + this.c);
-    return perimeter;
+    return parseFloat(this.a + this.b + this.c);
   }
   getArea() {
     const halfPerimeter = this.getPerimeter() / 2;
-    const area = parseFloat(Math.sqrt(halfPerimeter * (halfPerimeter - this.a) * (halfPerimeter - this.b) * (halfPerimeter - this.c)).toFixed(3));
-    return area;
+    return parseFloat(Math.sqrt(halfPerimeter * (halfPerimeter - this.a) * (halfPerimeter - this.b) * (halfPerimeter - this.c)).toFixed(3));
   }
 }
 function getTriangle(a, b, c) {
   try {
-    let newTriangle = new Triangle(a, b, c);
-    return newTriangle;
+    // let newTriangle = new Triangle(a, b, c);
+    return new Triangle(a, b, c);
   } catch (e) {
     return {
       getPerimeter() {
